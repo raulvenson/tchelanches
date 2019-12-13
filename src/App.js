87a48403e217073447287a99dfbca7010b5d3d17
Raul Venson <raulvenson@gmail.com>
2019-12-13 15:53:37 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+import Cardapio425px from './425px.png'
+
+
+function App() {
+  return (
+    <div>
+      
+      <img src={Cardapio425px} alt="Cardápio Tchê Lanches"/>
+      
+    </div>
+  );
+}
+
+export default App;
